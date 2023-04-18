75f08984de1aedadc67bfa3e657fbf88ccfb4103
3esserK <131145462+3sserK@users.noreply.github.com>
2023-04-18 11:00:56 +0200

    Klasse Spiel zugefügt

---
+++ MVC_Spiel_ReST/Models/Spiel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Spiel_ReST.Models
+{
+    public class Spiel
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public int MinSpieler { get; set; }
+        public int MaxSpieler { get; set; }
+        public string SpielPublisher { get; set; }
+    }
+}
