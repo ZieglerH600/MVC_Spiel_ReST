1ca0890164a10c637b8f9df4d5acdb5d4bd9a7e9
Fia <Fia@B410-114>
2023-04-18 10:14:54 +0200

    Projektdateien hinzufügen.

---
+++ MVC_Spiel_ReST/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace MVC_Spiel_ReST.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
