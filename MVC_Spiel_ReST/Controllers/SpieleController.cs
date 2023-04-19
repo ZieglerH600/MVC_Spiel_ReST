3d7be21401fca3c5e8950edb9f1bf6d0870384aa
3esserK <131145462+3sserK@users.noreply.github.com>
2023-04-19 14:59:44 +0200

    Delete geändert

--- MVC_Spiel_ReST/Controllers/SpieleController.cs
+++ MVC_Spiel_ReST/Controllers/SpieleController.cs
@@ -65,8 +65,9 @@ namespace MVC_Spiel_ReST.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteGame(int SIP)
         {
-            dataAccess.DeleteGameByID(SIP);
-            return new JsonResult(true);
+            bool geklappt = dataAccess.DeleteGameByID(SIP);
+            
+            return new JsonResult(geklappt);
 
         }
 
